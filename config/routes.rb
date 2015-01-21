2104ff6d18e553bd025ef6a1c393f0a13bf73f8f
divya sharma <divya.sharma@weboniselab.com>
2015-01-21 16:59:14 +0530

    Routes.rb modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
-  root 'static_pages#home'
+  root to: 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'help' => 'static_pages#help'
